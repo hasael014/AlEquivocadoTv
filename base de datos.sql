bbb7961dc2b81c9d02fdd9ea17b8e3d6bd78c7fc
hasael014 <79614326+hasael014@users.noreply.github.com>
2021-12-14 23:21:56 -0600

    ACTUALIZACION

---
+++ base de datos.sql
@@ -0,0 +1,38 @@
+CREATE TABLE RAMA_FAMILIARES(
+Integrantes varchar (30),
+Nombre varchar (20),
+Apellido varchar (30),
+Edad int (20),
+Domicilio varchar (40),
+Ciudad varchar (40),
+PRIMARY KEY (nombre)
+);
+
+--es la segunda parte
+
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Abuelo Paterno','Jose','Hernández Hernández','0','coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Abuela paterno','María','Hernández Hernández','0','coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Abuelo materno','Domingo','Hernández Robles','66','Coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Abuela materna','Maria Antonia','Hernández Hernández','66','coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Mi papa','Diego','Hernández Hernandez','39','Coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Mi mama','Concepción','Hernández Bautista','39','Coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Tio materna','Domingo','Hernández Bautista','39','Coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Tio materno','Felix','Hernández Bautista','37','Coamila','Huejutla');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Tio paterno','Jose Antonio','Hernández Hernandez','43','Coamila','Mexico');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Tia paterna','Claudia Magdalena','Hernández Hernandez','47','Coamila','Mexico');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Primo paterno','José Javino','Hernández zeron','17','Coamila','Mexico');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Primo materno','Antonia Mayeli','Hernández Hernández','15','Coamila','Mexico');
+INSERT INTO RAMA_FAMILIARES
+(Integrantes,Nombre,Apellido,Edad,Domicilio,Ciudad) VALUES ('Hijo','Cirilo','Hernández Hernandez','17','Coamila','Huejutla');
