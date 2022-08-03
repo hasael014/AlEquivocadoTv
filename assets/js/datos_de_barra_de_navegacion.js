ceaec8e368175edb8c601fbe84da72b2ff9001f7
hasael014 <79614326+hasael014@users.noreply.github.com>
2022-08-02 20:05:45 -0500

    v0.0.1

--- assets/js/datos_de_barra_de_navegacion.js
+++ assets/js/datos_de_barra_de_navegacion.js
@@ -1,11 +1,16 @@
-navegador = {
+const navegador = {
 	pagina1 : {
 		titulo : "al equivocado tv",
 		namelink1 : "cursos",
-		link1 : "#",
+		link1 : "cursos.html?pagina=2&name=cursos",
 		namelink2 : "codigos",
 		link2 : "#",
 		namelink3 : "descargas",
 		link3 : "#"
+	},
+	pagina2 : {
+		titulo : "cursos",
+		namelink1 : "inicio",
+		link1 : "index.html?pagina=1"
 	}
 }
--- assets/js/varmenu.js
+++ assets/js/varmenu.js
@@ -1,8 +1,40 @@
-PrintInWindow(navegador.pagina1.titulo, "title");
-PrintInWindow(navegador.pagina1.namelink1, "link1")
-PrintInWindow(navegador.pagina1.namelink2, "link2")
-PrintInWindow(navegador.pagina1.namelink3, "link3")
+//PrintInWindow(navegador.pagina1.titulo, "alequivocadotv")
+PrintInWindow(navegador.pagina2.titulo, "cursos")
+PrintInWindow(navegador.pagina2.namelink1, "inicio")
+PrintInWindow(navegador.pagina1.namelink1, "cursos")
+PrintInWindow(navegador.pagina1.namelink2, "codigos")
+PrintInWindow(navegador.pagina1.namelink3, "descargas")
 
-atributo("link1", navegador.pagina1.link1)
-atributo("link2", navegador.pagina1.link2)
-atributo("link3", navegador.pagina1.link3)
+atributo("inicio", navegador.pagina2.link1)
+atributo("cursos", navegador.pagina1.link1)
+atributo("codigos", navegador.pagina1.link2)
+atributo("descargas", navegador.pagina1.link3)
+
+
+/*
+
+const valores = window.location.search;
+
+//Mostramos los valores en consola:
+console.log(valores);
+
+//Resultado:
+//producto=camiseta&color=azul&talla=s
+
+//Creamos la instancia
+const urlParams = new URLSearchParams(valores);
+
+//Accedemos a los valores
+var producto = urlParams.get('producto');
+
+//Verificar si existe el parámetro
+console.log(urlParams.has('producto'));
+
+//Puedes recorrer los valores, claves y pares completos.
+const
+  keys = urlParams.keys(),
+  values = urlParams.values(),
+  entries = urlParams.entries();
+
+for (const value of values) console.log(value);
+//camiseta, azul, s*/
